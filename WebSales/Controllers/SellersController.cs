368b408116052a8970b6cb85bc9ed969926b97a4
Mateus Borba <mateus.neg25@gmail.com>
2024-03-18 00:30:03 -0300

    Validation

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -39,6 +39,13 @@ namespace WebSales.Controllers
         [AutoValidateAntiforgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel {Seller=seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
 
             return RedirectToAction(nameof (Index));
@@ -107,6 +114,14 @@ namespace WebSales.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) 
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel {Seller=seller, Departments = departments };
+                return View(viewModel);
+            }
+
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { Message = "Id mismatch" });
